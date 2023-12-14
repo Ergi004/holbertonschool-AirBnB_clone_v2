cf1a91236a2c197b4bc0ec3dd313f5ed65e2fdfc
Ergi Lama <ergilana98@gmail.com>
2023-12-14 11:38:56 +0100

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-'''
-Module to initiate flask
-'''
+"""
+Module to initiate a flask app
+"""
+
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    '''
+    """
     Index route
-    '''
-    return "Hello HBNB!"
+    """
+    return 'Hello HBNB!'
 
 
-if __name__ = "__main__":
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
